d52d338560ff930e4decffddce5346b5c1b68479
AnaLourenco3 <108146714+AnaLourenco3@users.noreply.github.com>
2022-12-21 13:44:03 +0100

    Uploading images to Cloudinary works

--- src/App.js
+++ src/App.js
@@ -2,28 +2,34 @@ import "./App.css";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getUserWithStoredToken } from "./store/user/thunks";
-import { Routes, Route } from "react-router-dom";
-import { Navigation, MessageBox } from "./components";
-import { Homepage, Login, SignUp } from "./pages"
+// import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import Upload from "./pages/Upload.js";
 
+import { Navigation, MessageBox } from "./components";
+// import { Homepage, Login, SignUp } from "./pages"
 
 function App() {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getUserWithStoredToken());
   }, [dispatch]);
-  
+
   return (
     <div>
-      <Navigation/>
-      <MessageBox/>
-      <Routes>
+      <Navigation />
+      <MessageBox />
+      <div className="container">
+        <Routes>
+          <Route path="/upload" element={<Upload />} />
+        </Routes>
+      </div>
+      {/* <Routes>
         <Route path="/" element={<Homepage />}/>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
