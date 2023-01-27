712db615ff17b7995fb9b743fb888c6477097e84
AnaLourenco3 <108146714+AnaLourenco3@users.noreply.github.com>
2023-01-27 17:51:41 +0000

    added render back

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const apiUrl = "http://localhost:4000";
+export const apiUrl =
+  "https://mieventsbackend-analourenco3.onrender.com" ||
+  "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 5000;
-
-//   "https://mieventsbackend-analourenco3.onrender.com" ||
